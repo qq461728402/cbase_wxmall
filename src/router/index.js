0bb67fe0bc5f77ccb007fbfd1a4576136bd0cff2
付小勇 <237468682@qq.com>
2018-03-06 16:06:57 +0800

    团购

--- src/router/index.js
+++ src/router/index.js
@@ -53,6 +53,7 @@ const Index = resolve => require.ensure([], () => resolve(require('../components
 const MyPoints = resolve => require.ensure([], () => resolve(require('../components/pageHome/MyPoints/MyPoints')), 'MyPoints')
 const PointsList = resolve => require.ensure([], () => resolve(require('../components/pageHome/MyPoints/PointsList')), 'PointsList')
 const PointsDetails = resolve => require.ensure([], () => resolve(require('../components/pageHome/MyPoints/PointsDetails')), 'PointsDetails')
+const GroupBuying = resolve => require.ensure([], () => resolve(require('../components/pageHome/GroupBuying/GroupBuying')), 'GroupBuying')
 
 Vue.use(Router)
 const router = new Router({
@@ -118,6 +119,7 @@ const router = new Router({
 	{path:'/home/MyPoints',name:'MyPoints',component:MyPoints,meta:{title:'积分中心',keepAlive:true}},
     {path:'/home/PointsList',name:'PointsList',component:PointsList,meta:{title:'积分中心',keepAlive:true}},
     {path:'/home/PointsDetails',name:'PointsDetails',component:PointsDetails,meta:{title:'商品详情',keepAlive:true}},
+	{path:'/home/GroupBuying',name:'GroupBuying',component:GroupBuying,meta:{title:'团购频道',keepAlive:true}},
   ]
 })
 export default router
