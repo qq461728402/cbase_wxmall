19037ca14fe8bd464c10deefc2fe40f6c7bc0909
付小勇 <237468682@qq.com>
2018-03-15 15:15:03 +0800

    gai

--- src/components/personalCenter/salesReturn/fillsalesRetrun.vue
+++ src/components/personalCenter/salesReturn/fillsalesRetrun.vue
@@ -11,22 +11,42 @@
       <yd-cell-item>
         <span slot="left" class="or_2">订单编号：{{orderItems.number}}</span>
       </yd-cell-item>
-      <div v-for="item,index in orderItems.item" :key="index" style="padding: 0.2rem">
-        <goods :item="item" goodsType="submit" :showQuantity="true"></goods>
-      </div>
+      <yd-flexbox v-for="item,index in orderItems.item" :key="index">
+        <div class="or_4">
+          <img class="or_5" :src="item.imageUrl">
+        </div>
+        <yd-flexbox-item class="or_6">
+          <yd-flexbox direction="vertical" style="padding-right: 0.3rem">
+            <yd-flexbox-item><p class="or_8">{{item.skuName}}</p></yd-flexbox-item>
+            <yd-flexbox-item>
+              <yd-flexbox>
+                <yd-flexbox-item><span class="or_9" style="color: #d41d0f">&yen;{{item.salePrice}}</span></yd-flexbox-item>
+                <yd-flexbox-item style="text-align: right"><span class="or_9">x{{item.quantity}}</span></yd-flexbox-item><!--件数-->
+              </yd-flexbox>
+            </yd-flexbox-item>
+          </yd-flexbox>
+        </yd-flexbox-item>
+      </yd-flexbox>
     </yd-cell-group>
-    <yd-cell-group style="margin-top: 0.2rem">
-      <yd-cell-item arrow type="label">
+
+    <yd-cell-group style="margin-top: 0.2rem;border-bottom: 1px solid #eeeeee;">
+      <div class="type">
+        <p>服务类型</p>
+        <yd-button class="anniu">维修</yd-button>
+        <yd-button class="anniu">退货</yd-button>
+        <yd-button class="anniu">换货</yd-button>
+      </div>
+      <!--<yd-cell-item arrow type="label">
         <span slot="left">服务类型：</span>
         <select slot="right" style="color: black" v-model="reasons" ref="reason">
           <option value="退货">退货</option>
           <option value="换货">换货</option>
           <option value="维修">维修</option>
         </select>
-      </yd-cell-item>
-      <yd-cell-item>
+      </yd-cell-item>-->
+      <!--<yd-cell-item>
         <yd-textarea slot="right" maxlength="100" placeholder="问题详细描述" v-model="dess" ref="desc"></yd-textarea>
-      </yd-cell-item>
+      </yd-cell-item>-->
       <yd-cell-item v-if="1==2">
         <span slot="left">用户名：</span>
         <yd-input slot="right"  max="20" placeholder="请输入用户名"  v-model="orderItems.contactName" ref="contactName"></yd-input>
@@ -37,6 +57,19 @@
       </yd-cell-item>
     </yd-cell-group>
 
+    <yd-cell-group style="margin-top: 10px" title="问题描述">
+      <yd-cell-item>
+        <yd-textarea slot="right" placeholder="请输入问题详细描述？" maxlength="100" v-model="dess" ref="desc"></yd-textarea>
+      </yd-cell-item>
+    </yd-cell-group>
+
+    <div class="type" style="margin-top: 10px;height: 3rem!important;">
+      <p>商品退回方式</p>
+      <yd-button class="anniu" style="width: 1.5rem!important;">快递至商城</yd-button>
+      <yd-button class="anniu" style="width: 1.5rem!important;color: #999999;border: 1px solid #999999;">送货至自提点</yd-button>
+      <p style="color: #999999;padding-top: .2rem!important;">商品寄回地址将在审核通过后以短信形式告知，或在申请记录中查询。商城不收取快递附加费。</p>
+    </div>
+
     <yd-button-group>
       <yd-button size="large" class="thlb7" @click.native="submit()">提交申请</yd-button>
     </yd-button-group>
@@ -46,11 +79,8 @@
 <script type="text/babel">
   import {baseHttp,getCookie,uploadURL} from '../../../config/env'
   import  {getStore,removeStore} from '../../../config/mUtils'
-  import  goods from '@/views/goods'
+
   const vm= {
-    components: {
-      goods,
-    },
     data() {
       return {
         orderItems:{},
@@ -80,6 +110,7 @@
       gotoback(){
         this.$router.go(-1);
       },
+
       submit(){
         const input = this.$refs.contactPhone;//电话号码
         this.orderItems.desc=this.reasons+"|"+this.dess;
@@ -167,5 +198,31 @@
   #fillsales button.thlb7.yd-btn-block.yd-btn-primary{
     background-color:#d41d0f ;
     font-size: .3rem;
+  }
+  .type{
+    height: 2rem;
+    padding-left: .3rem;
+    background-color: #ffffff;
+  }
+  .type p{
+    font-size: .3rem;
+    padding-top: .3rem;
+    padding-bottom: .2rem;
+  }
+  .type .anniu{
+    border-radius:0;
+    background-color: #ffffff;
+    color: red;
+    border: 1px solid red;
+    width: 1rem;
+    font-size: .25rem;
+  }
+
+  .yd-cell-title{
+    padding-top: .2rem;
+    padding-bottom: .2rem;
+    padding-left: .3rem;
+    background-color: #ffffff;
+    color: #000000;
   }
 </style>
