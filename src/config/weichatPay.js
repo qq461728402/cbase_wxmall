81499a2df444d5ff657ab0ff1d41b83913865d0f
qq461728402 <461728402@qq.com>
2018-04-11 13:50:31 +0800

    首页修改

--- src/config/weichatPay.js
+++ src/config/weichatPay.js
@@ -75,7 +75,7 @@ export  function getLocation(data,cb,errorCb) {
   });
   wx.error(function(res) {
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-    alert("config信息验证失败");
+    alert(res);
   });
 }
 
@@ -109,7 +109,7 @@ export  function openLocation(data,storeInfo,cb,errorCb) {
   });
   wx.error(function(res) {
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-    alert("config信息验证失败");
+    alert(res);
   });
 }
 //威富通支付
